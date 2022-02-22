d86c5e8f71225ed5e3d160540a5fca41f032cce0
charon <cariasjosue@gmail.com>
2022-02-21 20:23:41 -0500

    generated html file using node

--- app.js
+++ app.js
@@ -1,6 +1,52 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log (profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // this..
 const printProfileData = profileDataArr => {
     for (let i = 0; i < profileDataArr.length; i++) {
@@ -16,3 +62,4 @@ const printProfileData = profileDataArr => {
   
 
 printProfileData(profileDataArgs);
+*/
